ba13beec063105f0a6c41e3cb097bcdcd1e90d0c
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2021-12-11 18:15:55 -0300

    Updated db scripts

--- db-scripts/create-table-customer.sql
+++ db-scripts/create-table-customer.sql
@@ -2,6 +2,6 @@ CREATE TABLE customer(
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
     customer_name VARCHAR(255) NOT NULL,
     city_id int NOT NULL,
-    CONSTRAINT name_gt_2_char CHECK (CHAR_LENGTH(customer_name) > 2),
+    CONSTRAINT customer_name_gt_2_char CHECK (CHAR_LENGTH(customer_name) > 2),
     CONSTRAINT city_relationship FOREIGN KEY (city_id) REFERENCES city(id)
 ) DEFAULT CHARSET UTF8 COMMENT '';
